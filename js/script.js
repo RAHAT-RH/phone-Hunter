f5e30ca05e5800d7a834a8eba6a1179bb2fcadfb
RAHAT-RH <alaminrahat12@gmail.com>
2022-03-01 00:42:50 +0600

    spinner function added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
+// Spinner Added Function
 const spinner = (remove, add) => {
   const spinnerBlink = document.getElementById('spinner-blink');
   spinnerBlink.classList.remove(remove);
   spinnerBlink.classList.add(add);
 }
 
-spinner(remove)
